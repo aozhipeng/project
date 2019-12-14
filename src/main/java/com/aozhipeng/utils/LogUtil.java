5513708c9f3396b13e7a140f8c203df54091774f
aozhipengjava@126.com <aozhipengjava@126.com>
2019-12-14 15:26:39 +0800

    ok

---
+++ src/main/java/com/aozhipeng/utils/LogUtil.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.aozhipeng.utils;
+
+/**
+ * @author aozhipeng
+ * 2019Äê12ÔÂ14ÈÕ
+ */
+public class LogUtil {
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
